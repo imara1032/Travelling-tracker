2cb059d0d7633747a166a49d7f5728d575478667
Mauro Greco <mgreco@csumb.edu>
2015-11-02 15:28:55 -0800

    First MySQL test.

---
+++ CST438Project/CST438Project/www/scripts/test.js
@@ -0,0 +1,20 @@
+﻿$('form').submit(function () {
+    var landmarkID = $(this).parent().attr('data-landmark-id');
+    var postData = $(this).serialize();
+
+    $.ajax({
+        type: 'POST',
+        data: postData + '&amp;lid=' + landmarkID,
+        url: 'http://localhost/test/test.php',
+        success: function (data) {
+            console.log(data);
+            alert('Your comment was successfully added');
+        },
+        error: function () {
+            console.log(data);
+            alert('There was an error adding your comment');
+        }
+    });
+
+    return false;
+});
